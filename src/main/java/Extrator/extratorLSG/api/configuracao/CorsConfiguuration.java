aaf7d8f72da2027546a8fc02f1f05cbdeef39e0d
Alana Araujo <alana.araujo35@gmail.com>
2022-01-25 20:19:11 -0300

    .

---
+++ src/main/java/Extrator/extratorLSG/api/configuracao/CorsConfiguuration.java
@@ -0,0 +1,16 @@
+package Extrator.extratorLSG.api.configuracao;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CorsConfiguuration extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS","HEAH","TRACE","CONNECT");
+    }
+}
--- src/main/java/Extrator/extratorLSG/api/controller/PessoasController.java
+++ src/main/java/Extrator/extratorLSG/api/controller/PessoasController.java
@@ -8,14 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+@CrossOrigin(origins="http://localhost:8081",maxAge = 3600)
 @RestController
 @RequestMapping("/pessoas")
 public class PessoasController {
